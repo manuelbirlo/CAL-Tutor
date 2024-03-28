78a8d3ae8f39f5c791e2fe122d08e04688d9b253
manuelbirlo <71711219+manuelbirlo@users.noreply.github.com>
2024-03-28 19:08:15 +0100

    Update DeactivateAllPlanesAtApplicationStart.cs

--- Assets/DeactivateAllPlanesAtApplicationStart.cs
+++ Assets/DeactivateAllPlanesAtApplicationStart.cs
@@ -11,8 +11,6 @@ public class DeactivateAllPlanesAtApplicationStart : MonoBehaviour
         var abdomenStandardPlane = GameObject.Find("AbdomenPlane");
         var femurStandardPlane = GameObject.Find("FemurPlane");
 
-        // csvReaderScript.DeactivateLineRendering();
-
         headStandardPlane.GetComponent<MeshRenderer>().enabled = false;
         abdomenStandardPlane.GetComponent<MeshRenderer>().enabled = false;
         femurStandardPlane.GetComponent<MeshRenderer>().enabled = false;
@@ -20,10 +18,6 @@ public class DeactivateAllPlanesAtApplicationStart : MonoBehaviour
         headStandardPlane.transform.Find("HeadPlaneMirrored").GetComponent<MeshRenderer>().enabled = false;
         abdomenStandardPlane.transform.Find("AbdomenPlaneMirrored").GetComponent<MeshRenderer>().enabled = false;
         femurStandardPlane.transform.Find("FemurPlaneMirrored").GetComponent<MeshRenderer>().enabled = false;
-
-        //babyModel.transform.Find("ProbeSidedEdgeLine_CloneUSPlane_1").GetComponent<LineRenderer>().enabled = false;
-        //babyModel.transform.Find("ProbeSidedEdgeLine_CloneUSPlane_2").GetComponent<LineRenderer>().enabled = false;
-        //babyModel.transform.Find("ProbeSidedEdgeLine_CloneUSPlane_3").GetComponent<LineRenderer>().enabled = false;
 
         headStandardPlane.transform.Find("HeadLabelForUsImage").GetComponent<MeshRenderer>().enabled = false;
         abdomenStandardPlane.transform.Find("AbdomenLabelForUsImage").GetComponent<MeshRenderer>().enabled = false;
