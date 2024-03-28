e077cbc862b38eaa91b0dc19e80f2a36b023d45d
manuelbirlo <71711219+manuelbirlo@users.noreply.github.com>
2024-03-28 19:21:54 +0100

    Update EyeTest.cs

--- Assets/EyeTest.cs
+++ Assets/EyeTest.cs
@@ -7,11 +7,6 @@ using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.Examples.Demos.EyeTracking
 {
-    /// <summary>
-    /// Sample for allowing a GameObject to follow the user's eye gaze
-    /// at a given distance of "DefaultDistanceInMeters".
-    /// </summary>
-
     public class EyeTest : MonoBehaviour
     {
        
